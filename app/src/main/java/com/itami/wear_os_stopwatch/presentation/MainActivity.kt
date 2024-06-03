03a6d5853c46e53b4c40ce2ed8d47d1365151935
Вадим <itamiomw@gmail.com>
2024-06-03 16:17:44 +0300

    Initial commit

---
+++ app/src/main/java/com/itami/wear_os_stopwatch/presentation/MainActivity.kt
@@ -0,0 +1,41 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+package com.itami.wear_os_stopwatch.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.TimeText
+import com.itami.wear_os_stopwatch.presentation.theme.WearOsStopwatchTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setTheme(android.R.style.Theme_DeviceDefault)
+
+        setContent {
+            WearOsStopwatchTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                    contentAlignment = Alignment.Center
+                ) {
+                    TimeText()
+                }
+            }
+        }
+    }
+}
