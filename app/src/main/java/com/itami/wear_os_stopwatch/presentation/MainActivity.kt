01d201b1f90d86610680d69c075b5350da10c1bd
Вадим <itamiomw@gmail.com>
2024-06-03 18:19:56 +0300

    Stopwatch implemented

--- app/src/main/java/com/itami/wear_os_stopwatch/presentation/MainActivity.kt
+++ app/src/main/java/com/itami/wear_os_stopwatch/presentation/MainActivity.kt
@@ -9,14 +9,37 @@ package com.itami.wear_os_stopwatch.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Stop
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.TimeTextDefaults
 import com.itami.wear_os_stopwatch.presentation.theme.WearOsStopwatchTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,17 +47,76 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setTheme(android.R.style.Theme_DeviceDefault)
-
         setContent {
+            val viewModel = viewModel<MainViewModel>()
+            val timerState by viewModel.timerState.collectAsStateWithLifecycle()
+            val stopwatchText by viewModel.stopWatchText.collectAsStateWithLifecycle()
             WearOsStopwatchTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background),
-                    contentAlignment = Alignment.Center
+                Scaffold(
+                    timeText = {
+                        TimeText(
+                            timeTextStyle = TimeTextDefaults.timeTextStyle(
+                                fontSize = 10.sp
+                            )
+                        )
+                    }
                 ) {
-                    TimeText()
+                    Stopwatch(
+                        timerState = timerState,
+                        text = stopwatchText,
+                        onToggleRunning = viewModel::toggleIsRunning,
+                        onReset = viewModel::resetTimer,
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Stopwatch(
+    timerState: TimerState,
+    text: String,
+    onToggleRunning: () -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = onToggleRunning) {
+                Icon(
+                    imageVector = if (timerState == TimerState.RUNNING) Icons.Default.Pause
+                    else Icons.Default.PlayArrow,
+                    contentDescription = null
+                )
+            }
+            Spacer(modifier = Modifier.width(6.dp))
+            Button(
+                onClick = onReset,
+                enabled = timerState != TimerState.RUNNING,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.surface,
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Stop,
+                    contentDescription = null
+                )
             }
         }
     }
---
+++ app/src/main/java/com/itami/wear_os_stopwatch/presentation/TimerState.kt
@@ -0,0 +1,5 @@
+package com.itami.wear_os_stopwatch.presentation
+
+enum class TimerState {
+    RUNNING, PAUSED, RESET
+}
